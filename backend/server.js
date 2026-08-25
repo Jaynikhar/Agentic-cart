a8c47c8461c169d7c20011aaa7c4e27862205c1f
Jaynikhar <jaynikhar61@gmail.com>
2026-08-25 16:47:15 +0530

    edit server.js

--- backend/server.js
+++ backend/server.js
@@ -32,22 +32,26 @@ connectDB().then(async () => {
 });
 
 // Middleware
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://agentic-cart-eight.vercel.app',
-];
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://agentic-cart-eight.vercel.app',
+// ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    const cleanOrigin = origin.replace(/\/$/, '');
-    if (allowedOrigins.includes(cleanOrigin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     const cleanOrigin = origin.replace(/\/$/, '');
+//     if (allowedOrigins.includes(cleanOrigin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// }));
+
+
+app.use(cors({ origin: 'https://agentic-cart-eight.vercel.app/' }));
+
 
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
